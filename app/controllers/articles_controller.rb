8223df8b25decf5e7fb69c8eb1d39ecc3b8d8017
mach1010 <jetstream1010@gmail.com>
2014-06-26 22:35:02 -0500

    added articles#edit/#update views, created partial for new/edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+    
     redirect_to article_path(@article)
   end
   
@@ -24,7 +25,10 @@ class ArticlesController < ApplicationController
   end
   
   def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
     
+    redirect_to article_path(@article)
   end
   
   def destroy
