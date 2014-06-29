4ac06e127ccb2339c8ded9c066dc01a19f460209
mach1010 <jetstream1010@gmail.com>
2014-06-29 14:45:55 -0500

    added tag index and tag delete functionality

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,4 +3,11 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
   end
+  
+  def destroy
+    Tag.find(params[:id]).destroy
+    
+    flash.notice = "Tag successfully deleted"
+    redirect_to tags_path
+  end
 end
