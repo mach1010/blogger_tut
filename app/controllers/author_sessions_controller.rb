028614af8bf74c961cc5abe4868adacd004839f8
mach1010 <jetstream1010@gmail.com>
2014-06-29 19:03:03 -0500

    added sorcery authentication, created author_sessions model and controller

---
+++ app/controllers/author_sessions_controller.rb
@@ -0,0 +1,17 @@
+class AuthorSessionsController < ApplicationController
+  
+  def new
+  end
+  
+  def create
+    if login(params[:email], params[:password])
+      redirect_back_or_to(articles_path, message: 'Logged in successfully')
+    else
+      Flash.now.alert = "Login failed."
+      render action: :new
+    end
+    
+    def destroy
+      logout
+      redirect_to(:authors, message: 'Logged out')
+end
